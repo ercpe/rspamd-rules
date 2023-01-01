e04019131edd6dcf2e93473b28f2bdb39563a780
Johann Schmitz <johann@j-schmitz.net>
2023-01-01 06:12:20 +0100

    Improve cryptobot matching

--- local.d/rspamd.lua
+++ local.d/rspamd.lua
@@ -5,16 +5,13 @@ cnf['FROM_MUNGAI'] = {
     score = 2.5,
 }
 
-local re_body_cryptobot = '/.*(crypto|auto|trader|new)bot.*/i{mime}'
-local re_subject_cryptobot = 'Subject=/.*(crypto|auto|trader|new)bot.*/i{header}'
-local re_body_tariff = '/.*tariff.*/i{mime}'
-local re_body_initial_deposit = '/.*initial deposit.*/i{mime}'
-cnf['BODY_CRYPTOBOT'] = {
-    re = string.format('((%s) || (%s)) && ((%s) || (%s))', re_body_cryptobot, re_subject_cryptobot, re_body_tariff, re_body_initial_deposit),
+cnf['SUBJECT_CRYPTOBOT'] = {
+    re = 'Subject=/.*(crypto|auto|trader|new)bot.*/i{header}',
     score = 1.0,
 }
-cnf['BODY_CRYPTOBOT_LAX'] = {
-    re = string.format('((%s) || (%s))', re_body_cryptobot, re_subject_cryptobot),
+
+cnf['BODY_CRYPTOBOT'] = {
+    re = '/.*(crypto|auto|trader|new)bot.*/i{mime}',
     score = 1.0,
 }
 
